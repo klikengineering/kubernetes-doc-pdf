38dae839cbf49d05f0c4b75a070c2998f66fcc23
Sourcery AI <>
2023-11-10 18:04:25 +0000

    'Refactored by Sourcery'

--- kubernetes-doc.py
+++ kubernetes-doc.py
@@ -22,24 +22,17 @@ def generate_directory_pdf(url, name, s=None):
     cwd = os.getcwd()
     for l1 in links:
         r2 = s.get(l1)
-        # r2.html.render()
-        div = r2.html.find('.td-content', first=True, clean=True)
-        if div:
-            # try:
-                # if name in ["Setup", "Tutorials", "Reference"]:  # will give duplicate id error, go through pages one by one to skip error page
-                    # print("testing " + l1, end='')
-                    # output = pypandoc.convert_text(div.html, "pdf", format="html", outputfile="/tmp/kubernetes_pdf_doc_tmp.pdf".format(name), extra_args=['--pdf-engine=weasyprint'])
-                    # print(" works")
+        if div := r2.html.find('.td-content', first=True, clean=True):
             # except Exception as e:
             #     print(" failed!")
             #     print(e)
             # else:
             html += div.html
-        with open("{}/{}.html".format(cwd, name), "wt") as f:
+        with open(f"{cwd}/{name}.html", "wt") as f:
             f.write(html)
 
     print("generating pdf...")
-    subprocess.run(["{}/weasy_print.sh".format(cwd), name])
+    subprocess.run([f"{cwd}/weasy_print.sh", name])
     # try:
     #     output = pypandoc.convert_text(html, "pdf", format="html", outputfile="./{}.pdf".format(name), extra_args=['--pdf-engine=weasyprint', '--css=codeblock_wrap.css'])
     # except Exception as e:
@@ -57,7 +50,9 @@ if __name__ == '__main__':
                    # "Admin",  # this direct to concepts
                    # "Imported",  # deprecated
                    ]
-    directories_pairs = [("https://kubernetes.io/docs/{}/".format(n.lower()), n) for n in directories]
+    directories_pairs = [
+        (f"https://kubernetes.io/docs/{n.lower()}/", n) for n in directories
+    ]
     for url, name in directories_pairs:
         print(name)
         generate_directory_pdf(url, name)
